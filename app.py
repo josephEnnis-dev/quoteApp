28a4c272a735a37c9beec5c8f775ec09d5d6365a
Joseph Ennis <jsennis@yahoo.com>
2025-07-28 17:42:26 +0800

    Intital commit with quote generator

---
+++ app.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Keep going. Everything you need will come to you at the perfect time.",
+    "Do something today that your future self will thank you for.",
+    "Believe you can and you're halfway there.",
+    "Dream big and dare to fail."
+]
+
+print("Quote of the Day:")
+print(random.choice(quotes))
